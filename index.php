63340dee00614a45e193fff9c03aff0919c85c11
Veliana1 <72461068+Veliana1@users.noreply.github.com>
2020-11-22 18:39:18 -0800

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 //echo '<p>This is PHP?</p>';
 
 if(isset($_GET["FirstName"])){//show feedback
-  echo $_GET["FirstName"]
+  echo $_GET["FirstName"];
 }else{//show form
   echo '
-  <form action=''>
+  <form action="">
   First Name: <input type="text" name="FirstName" /><br />
   </form>
   ';
